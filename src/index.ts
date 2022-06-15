3dcc85b21f40ce4a9c2b08fb5f72c50a3c53a285
Konsta Nenonen <konstanenonen@protonmail.com>
2022-06-15 23:46:34 +0300

    Done with basic configurations

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+console.log("Hello, World!!");
+
+const one: number = 1;
+
+console.log(one);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+};
