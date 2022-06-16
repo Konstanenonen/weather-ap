128c4d1403e3dc9e96a70b3fa224d268cc83e7b8
Konsta Nenonen <konstanenonen@protonmail.com>
2022-06-16 21:18:14 +0300

    Improved the ux by hiding result sections before first search

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,16 @@ locationForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   try {
+    weatherSection.classList.remove("hidden");
     weatherSection.innerText = "Loading...";
     const location: string = countryField.value;
+
     const weatherData = await Weather.getWeather(location);
     weatherSection.innerHTML = Weather.render(weatherData);
+
     const imageUrl = await ImageSearch.getPictureUrl(weatherData.weather);
     weatherImage.src = imageUrl;
+    weatherImage.classList.remove("hidden");
   } catch (error) {
     weatherSection.innerText = "Didn't find that location :(";
     console.log(`Error in the event listener: ${error}`);
